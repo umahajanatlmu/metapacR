15ad3f0bfe03d56800df1563b4b4891ce14d97e4
Ujjwal Mahajan <ujjwal_mukund.mahajan@med.uni-muenchen.de>
2023-02-09 14:20:43 +0100

    add data

---
+++ R/chemicalMetadata.R
@@ -0,0 +1,31 @@
+#' chemical metadata for annotation of metabolon
+#'
+#' contains a dataframe with metadata
+#'
+#' @format
+#' \describe{
+#'         \item{CHEM_ID}{Chemical id}
+#'         \item{LIB_ID}{library id}
+#'         \item{COMP_ID}{CCompound id}
+#'         \item{CHRO_LIB_ENTRY_ID}{CHRO_LIB_ENTRY_ID}
+#'         \item{SUPER_PATHWAY}{ONTOLOGY CLASS}
+#'         \item{SUB_PATHWAY}{ONTOLOGY SUBCLASS}
+#'         \item{PATHWAY_SORTORDER}{pathway sorting}
+#'         \item{TYPE}{Named / unnamed}
+#'         \item{INCHIKEY}{INCHIKEY}
+#'         \item{SMILES}{SMILES}
+#'         \item{CHEMICAL_NAME}{Chemical name}
+#'         \item{PLOT_NAME}{Chemical name}
+#'         \item{CAS}{CAS id}
+#'         \item{CHEMSPIDER}{Chemspider id}
+#'         \item{HMDB}{HMDB id}
+#'         \item{KEGG}{KEGG id}
+#'         \item{PUBCHEM}{PUBCHEM id}
+#'         \item{PLATFORM}{used platform}
+#'          }
+#' @source {metabolon}
+#'
+#' @examples
+#' data(chemicalMetadata)
+"chemicalMetadata"
+
---
+++ R/chemicalMetadata_MH.R
@@ -0,0 +1,22 @@
+#' chemical metadata for annotation of metanomics health
+#'
+#' contains a dataframe with metadata
+#'
+#' @format
+#' \describe{
+#'         \item{MET_CHEM_NO}{Chemical id}
+#'         \item{UNIQUE_ANALYTE_ID}{unique ID}
+#'         \item{ASSAY}{Assay Name}
+#'         \item{METABOLITE_NAME}{metabolite name}
+#'         \item{ONTOLOGY1_NAME}{ONTOLOGY CLASS}
+#'         \item{ONTOLOGY2_NAME}{ONTOLOGY SUBCLASS}
+#'         \item{CAS_UID}{CAS ID}
+#'         \item{DICT_STRUCTURE_ELUCIDATION_ID}{structurre}
+#'         \item{KEGG}{KEGG ID}
+#'          }
+#' @source {metanomics health}
+#'
+#' @examples
+#' data(chemicalMetadata_MH)
+"chemicalMetadata_MH"
+
